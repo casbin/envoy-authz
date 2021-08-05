c153e92e05de74ee32af5012d2acf5bde787d4e7
Ashish Malik <b218008@iiit-bh.ac.in>
2021-08-05 19:09:18 +0530

    Updated alignments
    Signed-off-by: Ashish Malik <b218008@iiit-bh.ac.in>

--- authz.go
+++ authz.go
@@ -6,12 +6,17 @@ import (
 	"log"
 	"net"
 	"os"
+	//"encoding/json"
 	"strings"
+	"encoding/base64"
 
+	//"github.com/dgrijalva/jwt-go"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"	
-	"github.com/golang/glog"
-
+	//"github.com/golang/glog"
+	"github.com/casbin/casbin"
+	rpcstatus "google.golang.org/genproto/googleapis/rpc/status"
+	envoy_type "github.com/envoyproxy/go-control-plane/envoy/type/v3"
 	auth "github.com/envoyproxy/go-control-plane/envoy/service/auth/v3"
 	core "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	"github.com/gogo/googleapis/google/rpc"
@@ -25,20 +30,98 @@ var (
 
 type AuthorizationServer struct{}
 
+/*func (a *AuthorizationServer) GetUserName(req *http.Request) string {
+	username, _, _ := req.BasicAuth()
+	return username
+}
+*/
+
 func (a *AuthorizationServer) Check(ctx context.Context, req *auth.CheckRequest) (*auth.CheckResponse, error) {
 	log.Println(">>> Server Performing authorization check!")
 	
-	e, err := casbin.NewEnforcer("./example/model.conf", "./example/policy.csv")
-	if err != nil {
+	e:= casbin.NewEnforcer("./example/model.conf", "./example/policy.csv")
+/*	if err != nil {
 		glog.Errorf("Filed to load the policies: %v", err)
-		return
+		
 	}
-
-	user, _, _ := req.BasicAuth()
+*/
+/*	user, _, _ := req.BasicAuth()
 	method := req.Method
 	path := req.URL.Path
+*/
+//	user :=  a.GetUserName(req) 
+//	user, _, _ := json.Unmarshal(req.Attributes.Request.Http.Headers["authorization"].BasicAuth())
 
-	if e.Enforce(user, path, method) {
+	method := req.Attributes.Request.Http.Method
+	path := req.Attributes.Request.Http.Path
+	authHeader, ok := req.Attributes.Request.Http.Headers["authorization"]
+	if !ok {
+		log.Fatalf("failed to receive headers")
+	}
+	var splitToken []string
+	splitToken = strings.Split(authHeader, "Bearer ")
+	if len(splitToken) == 2 {
+		token := splitToken[1]
+		tokenbyte, err := base64.StdEncoding.DecodeString(token)
+		if err != nil {
+			panic("malformed input")
+		}
+		tokenStr :=  string(tokenbyte[:])
+		tokenvalue := strings.Split(tokenStr, ",")
+		username := tokenvalue[1]
+		if e.Enforce(username, path, method) {
+			return &auth.CheckResponse{
+				Status: &rpcstatus.Status{
+					Code: int32(rpc.OK),
+				},
+				HttpResponse: &auth.CheckResponse_OkResponse{
+					OkResponse: &auth.OkHttpResponse{
+						Headers: []*core.HeaderValueOption{
+							{
+								Header: &core.HeaderValue{
+									Key:   "casbin-authorized",
+									Value: "allowed",
+								},
+							},
+						},
+					},
+				},
+			}, nil
+		}else {
+			return &auth.CheckResponse{
+				Status: &rpcstatus.Status{
+					Code: int32(rpc.PERMISSION_DENIED),
+				},
+				HttpResponse: &auth.CheckResponse_DeniedResponse{
+					DeniedResponse: &auth.DeniedHttpResponse{
+						Status: &envoy_type.HttpStatus{
+							Code: envoy_type.StatusCode_Unauthorized,
+						},
+						Body: "PERMISSION_DENIED",
+					},
+				},
+			}, nil
+	
+		}
+	}
+	return &auth.CheckResponse{
+		Status: &rpcstatus.Status{
+			Code: int32(rpc.UNAUTHENTICATED),
+		},
+		HttpResponse: &auth.CheckResponse_DeniedResponse{
+			DeniedResponse: &auth.DeniedHttpResponse{
+				Status: &envoy_type.HttpStatus{
+					Code: envoy_type.StatusCode_Unauthorized,
+				},
+				Body: "Authorization Header malformed or not provided",
+			},
+		},
+	}, nil
+	
+	//tokenvalue := strings.Split(tokenStr, ",")
+	//username := tokenvalue[1]
+	/*
+	if e.Enforce(username, path, method) {
 		return &auth.CheckResponse{
 			Status: &rpcstatus.Status{
 				Code: int32(rpc.OK),
@@ -56,8 +139,7 @@ func (a *AuthorizationServer) Check(ctx context.Context, req *auth.CheckRequest)
 				},
 			},
 		}, nil
-	}
-	else {
+	}else {
 		return &auth.CheckResponse{
 			Status: &rpcstatus.Status{
 				Code: int32(rpc.PERMISSION_DENIED),
@@ -72,25 +154,28 @@ func (a *AuthorizationServer) Check(ctx context.Context, req *auth.CheckRequest)
 			},
 		}, nil
 
-	}
+	}*/
+}
 
-func main(){
-	flag.Parse()
-	
-	if *grpcport == "" {
-		fmt.Fprintln(os.Stderr, "missing -grpcport flag ")
-		flag.Usage()
-		os.Exit(2)
-	}
-	lis, err := net.Listen("tcp", *grpcport)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
 
-	s := grpc.NewServer()
+	func main(){
 
-	auth.RegisterAuthorizationServer(s, &AuthorizationServer{})
+		flag.Parse()
+		
+		if *grpcport == "" {
+			fmt.Fprintln(os.Stderr, "missing -grpcport flag ")
+			flag.Usage()
+			os.Exit(2)
+		}
+		lis, err := net.Listen("tcp", *grpcport)
+		if err != nil {
+			log.Fatalf("failed to listen: %v", err)
+		}
 
-	log.Printf("Starting gRPC Server at %s", *grpcport)
-	s.Serve(lis)
+		s := grpc.NewServer()
+
+		auth.RegisterAuthorizationServer(s, &AuthorizationServer{})
+
+		log.Printf("Starting gRPC Server at %s", *grpcport)
+		s.Serve(lis)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/casbin/envoy-authz
 go 1.13
 
 require (
+	github.com/casbin/casbin v1.9.1 // indirect
+	github.com/casbin/casbin/v2 v2.35.0 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/envoyproxy/go-control-plane v0.9.9 // indirect
 	github.com/gogo/googleapis v1.4.1 // indirect
 	github.com/golang/glog v0.0.0-20210429001901-424d2337a529 // indirect
